fbcd7f6fe2f4fa518d7e1da027be20fd3dd61681
lazy_enzo <263153193+Aseel012@users.noreply.github.com>
2026-08-29 21:04:43 +0530

    Clean up formatting in format.ts
    Remove unnecessary newline in format.ts

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -11,7 +11,6 @@ export function formatTime(date: Date, format: TimeFormat, timeZone?: string) {
   return new Intl.DateTimeFormat("en-US", opts).format(date)
 }
 
-
 export function formatShortTime(date: Date, format: TimeFormat, timeZone?: string) {
   const opts: Intl.DateTimeFormatOptions = {
     hour: "2-digit",
